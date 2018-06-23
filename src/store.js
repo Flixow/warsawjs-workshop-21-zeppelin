359d680281b5bbb7196bc676858686a6bb63e3e9
Daniel Siwek <danielsiwek95@gmail.com>
2018-06-23 11:25:52 +0200

    Setup redux

---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(thunk)
+);
+
+const store = createStore(reducers, enhancer);
+
+export default store;
